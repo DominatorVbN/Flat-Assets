f25be2dcf501d1608cc3a29c943a2664faef2677
DominatorVbN <voilentbynature@gmail.com>
2020-12-19 22:47:35 +0530

    Initial Commit

---
+++ Shared/Flat_AssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flat_AssetsApp.swift
+//  Shared
+//
+//  Created by Amit Samant on 19/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Flat_AssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
